884aa501cd6a0881bc899be2cde0b94b066b0c37
Pawandeep Singh Thandi <81608228+Pawandeep16@users.noreply.github.com>
2022-02-10 20:13:34 +0530

    Update

--- even.py
+++ even.py
@@ -64,12 +64,12 @@ for images in os.listdir(folder_dir):
 
 
 clips=[]
-
+#Bluring Effect Function
 def blur(image):
     """ Returns a blurred (radius=4 pixels) version of the image """    
     return gaussian(image.astype(float), sigma=4)
 
-
+#Fetching the Texts and its properties from the file.csv
 for list  in  rows:
      text_clip=TextClip(txt ="\'"+list[1]+"\'",color= list[3],fontsize = int(list[2])  )
      text_clip=text_clip.set_position("center")
@@ -82,6 +82,7 @@ for list  in  rows:
      final_clip=final_clip.set_duration(2).crossfadeout(2.0)
      clips.append(final_clip)
 
+#Fetching the Images from the folder Image    
 for i in range(len(image)):
      filepath = "images/"+image[i]
      img = Image.open(filepath)
@@ -99,9 +100,10 @@ for i in range(len(image)):
      final=final.set_duration(2).crossfadein(2.0)
      clips.insert(2*i+1,final) 
 
-
+#Putting the all clips together to make it a ready for rendering 
 video_clip = concatenate_videoclips(clips,method="compose")
 
+#Rendering the  video for output Review and getting the data from the file name.csv 
 for list in rowsN:
     video_clip1=video_clip.resize(width=int(list[4]),height=int(list[5]))
     video_clip1.write_videofile( list[0] , fps= int(list[1]), remove_temp=True, codec=list[2], audio_codec=list[3])
